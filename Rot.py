9e7d867ec2538ee771355d8efa6c9708a7ce5773
John Williams <ponix4k@gmail.com>
2019-02-15 18:46:47 +0000

    Created inital Rot.py
    This is the python file that will encode or decode text with a rotation

---
+++ Rot.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+Alpha_Low = ('abcdefghijklmnopqrstuvwxyz')
+Alpha_Up  = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+
+newMessage = ''
+message = input('Please enter a message to encode/decode: ')
+Rot = input('Please Enter Rotation: ')
+try:
+ RotVal = int(Rot)
+ print('Shift vaulue is: ',RotVal)
+except ValueError:
+ print('This is not an number please try again')
+ Rot = input('Please Enter Rotation: ')
+
+#Lowercase Search
+for Char in message:
+ if Char in Alpha_Low:
+  position = Alpha_Low.find(Char)
+  newPosition = (position + RotVal) % 26
+  newChar = Alpha_Low[newPosition]
+  #print('The New character is is: ',newChar)
+  newMessage += newChar
+  
+  #Uppercase Search
+ elif Char in Alpha_Up:
+  position = Alpha_Up.find(Char)
+  newPosition = (position + RotVal) % 26
+  newChar = Alpha_Up[newPosition]
+  #print('The New character is is: ',newChar)
+  newMessage += newChar
+ else:
+  newMessage += Char
+print('Encoded/Decoded message is: ',newMessage)
