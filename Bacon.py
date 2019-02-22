2d4a843348ebc381c16a6d8583175d722c316cae
ponix4k <ponix4k@gmail.com>
2019-02-22 00:14:34 +0000

    added both 24 and 26 charsets to the app

--- Bacon.py
+++ Bacon.py
@@ -11,25 +11,29 @@ Dict24 = {'a' : 'aaaaa','b' : 'aaaab','c' : 'aaaba','d' : 'aaabb','e' : 'aabaa',
 'D' : 'AAABB','E' : 'AABAA','F' : 'AABAB','G' : 'AABBA','H' : 'AABBB','I' : 'ABAAA',
 'J' : 'ABAAA','K' : 'ABAAB','L' : 'ABABA','M' : 'ABABB','N' : 'ABBAA','O' : 'ABBAB',
 'P' : 'ABBBA','Q' : 'ABBBB','R' : 'BAAAA','S' : 'BAAAB','T' : 'BAABA','U' : 'BAABB',
-'V' : 'BAABB','W' : 'BABAA','X' : 'BABAB','Y' : 'BABBA','Z' : 'BABBB'}
+'V' : 'BAABB','W' : 'BABAA','X' : 'BABAB','Y' : 'BABBA','Z' : 'BABBB',
+'1' : '1','2' : '2','3' : '3','4' : '4','5' : '5','6' : '6','7' : '7','8' : '8',
+'9' : '9','0' : '0'}
 
-#Dict26 = {'A' : 'AAAAA','B' : 'AAAAB','C' : 'AAABA','D' : 'AAABB','E' : 'AABAA',
-#	'F' : 'AABAB','G' : 'AABBA','H' : 'AABBB','I' : 'ABAAA','J' : 'AVAAB',
-#	'K' : 'ABABA','L' : 'ABABB','M' : 'ABBAA','N' : 'ABBAB','O' : 'ABBBA',
-#	'P' : 'ABBBB','Q' : 'BAAAA','R' : 'BAAAB','S' : 'BAABA','T' : 'BAABB',
-#	'U' : 'BABAA','V' : 'BABAB','W' : 'BABBA','X' : 'BABBB','Y' : 'BBAAA',
-#	'Z' : 'BBAAB','a' : 'AAAAA','b' : 'AAAAB','c' : 'AAABA','d' : 'AAABB',
-#	'e' : 'AABAA','f' : 'AABAB','g' : 'AABBA','h' : 'AABBB','i' : 'ABAAA',
-#	'j' : 'AVAAB','k' : 'ABABA','l' : 'ABABB','m' : 'ABBAA','n' : 'ABBAB',
-#	'o' : 'ABBBA','p' : 'ABBBB','q' : 'BAAAA','r' : 'BAAAB','s' : 'BAABA',
-#	't' : 'BAABB','u' : 'BABAA','v' : 'BABAB','w' : 'BABBA','x' : 'BABBB',
-#	'y' : 'BBAAA','z' : 'BBAAB','1' : '1','2' : '2','3' : '3','4' : '4',
-#	'5' : '5','6' : '6','7' : '7','8' : '8','9' : '9','0' : '0'
-#	}
+Dict26 = {'A' : 'AAAAA','B' : 'AAAAB','C' : 'AAABA','D' : 'AAABB','E' : 'AABAA',
+	'F' : 'AABAB','G' : 'AABBA','H' : 'AABBB','I' : 'ABAAA','J' : 'AVAAB',
+	'K' : 'ABABA','L' : 'ABABB','M' : 'ABBAA','N' : 'ABBAB','O' : 'ABBBA',
+	'P' : 'ABBBB','Q' : 'BAAAA','R' : 'BAAAB','S' : 'BAABA','T' : 'BAABB',
+	'U' : 'BABAA','V' : 'BABAB','W' : 'BABBA','X' : 'BABBB','Y' : 'BBAAA',
+	'Z' : 'BBAAB','a' : 'AAAAA','b' : 'AAAAB','c' : 'AAABA','d' : 'AAABB',
+	'e' : 'AABAA','f' : 'AABAB','g' : 'AABBA','h' : 'AABBB','i' : 'ABAAA',
+	'j' : 'AVAAB','k' : 'ABABA','l' : 'ABABB','m' : 'ABBAA','n' : 'ABBAB',
+	'o' : 'ABBBA','p' : 'ABBBB','q' : 'BAAAA','r' : 'BAAAB','s' : 'BAABA',
+	't' : 'BAABB','u' : 'BABAA','v' : 'BABAB','w' : 'BABBA','x' : 'BABBB',
+	'y' : 'BBAAA','z' : 'BBAAB','1' : '1','2' : '2','3' : '3','4' : '4',
+	'5' : '5','6' : '6','7' : '7','8' : '8','9' : '9','0' : '0'
+	}
 
 
-print('V1.01')
-print('This Version will accept numbers and both uppercase and lowercase letters')
+print('V1.20')
+print('This Version will accept numbers and both uppercase and lowercase letters and has 24 and 26 character options')
+charset =input('Select charset (0 = 24char , 1 = 26char): ')
+charsetVar = int(charset)
 print('Do you want to encode or decode a messages ?')
 enc = input('Please make your selection (0 = encode 1 = decode): ')
 encvar = int(enc)
@@ -40,17 +44,20 @@ message = input('Please enter a message to Encode/Decode: ') #request message fr
 
 #function To Encode into the bacon cipher
 def encrypt(message):
-	cipher = ''
-	for letter in message:
-		if(letter != ' '):  #Check message array for a space
-			cipher += Dict24[letter]  # find the letter in the dictionary
-		else:
-			cipher += ' '
-	return cipher
+    cipher = ''
+    for letter in message:
+        if(letter != ' '):  #Check message array for a space
+            if charsetVar > 0:
+                 cipher += Dict26[letter]  # find the letter in the dictionary
+            else:
+                cipher += Dict24[letter]  # find the letter in the dictionary
+        else:
+            cipher += ' '
+    return cipher
 
 
-	# Function to decrypt the string
-	# according to the cipher provided
+    # Function to decrypt the string
+    # according to the cipher provided
 
 def decrypt(message):
     decipher = ''
@@ -67,13 +74,11 @@ def decrypt(message):
             # checking for space as the first
             # character of the substring
             if(substr[0] != ' '):
-                '''
-                This statement gets us the key(plaintext) using the values(ciphertext)
-                Just the reverse of what we were doing in encrypt function
-                '''
-                decipher += list(Dict24.keys())[list(Dict24.values()).index(substr)]
+                if charsetVar > 0:
+                    decipher += list(Dict26.keys())[list(Dict26.values()).index(substr)]
+                else:
+                    decipher += list(Dict24.keys())[list(Dict24.values()).index(substr)]
                 i += 5 # to get the next set of ciphertext
-
             else:
                 decipher += ' '
                 i += 1 # index next to the space
@@ -83,12 +88,12 @@ def decrypt(message):
     return decipher
 
 def main():
-	if encvar > 0:
-		result = decrypt(message.upper())
-		print ('Decoded message is: ',result)
-	else:
-		result = encrypt(message.upper())
-		print ('Encode message is: ',result)
+    if encvar > 0:
+        result = decrypt(message.upper())
+        print ('Decoded message is: ',result)
+    else:
+        result = encrypt(message.upper())
+        print ('Encode message is: ',result)
 
 if __name__ == '__main__':
-	main()
+    main()
