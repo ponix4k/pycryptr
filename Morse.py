feca36dee29c0c6a6bab80cd8ef25c71127d406d
ponix4k <ponix4k@gmail.com>
2019-10-02 13:10:38 +0100

    Fixed the issue with the morse not printing

--- Morse.py
+++ Morse.py
@@ -1,49 +1,91 @@
 #!/usr/bin/python3
-DICT_Morse = {'A':'.-','B':'-...','C':'-.-.','D':'-..','E':'.','F':'..-.','G':'--.',
-              'H':'....','I':'..','J':'.---','K':'-.-','L':'.-..','M':'--','N':'-.',
-              'O':'---','P':'.--.','Q':'--.-','R':'.-.','S':'...','T':'-','U':'..-',
-              'V':'...-','W':'.--','X':'-..-','Y':'-.--','Z':'--..','1':'.----',
-              '2':'..---','3':'...--','4':'....-','5':'.....','6':'-....',
-              '7':'--...','8':'---..','9':'----.','0':'-----',',':'--..--',
-              '.':'.-.-.-','?':'..--..','/':'-..-.','-':'-....-','(':'-.--.',')':'-.--.-'}
-
-enc = input('Please make your selection (0 = encode 1 = decode): ')
+# Dictionary representing the morse code chart 
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...', 
+                    'C':'-.-.', 'D':'-..', 'E':'.', 
+                    'F':'..-.', 'G':'--.', 'H':'....', 
+                    'I':'..', 'J':'.---', 'K':'-.-', 
+                    'L':'.-..', 'M':'--', 'N':'-.', 
+                    'O':'---', 'P':'.--.', 'Q':'--.-', 
+                    'R':'.-.', 'S':'...', 'T':'-', 
+                    'U':'..-', 'V':'...-', 'W':'.--', 
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..', 
+                    '1':'.----', '2':'..---', '3':'...--', 
+                    '4':'....-', '5':'.....', '6':'-....', 
+                    '7':'--...', '8':'---..', '9':'----.', 
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-', 
+                    '?':'..--..', '/':'-..-.', '-':'-....-', 
+                    '(':'-.--.', ')':'-.--.-'} 
+enc = input('Please Make Your Selection (0 = Encode, 1 = Decode): ')
 encVar = int(enc)
-
-message = input('Please enter a message to Encode/Decode: ')
-def encrypt(message):
-    cipher = ''
-    for letter in message:
-        if letter != ' ':
-            cipher += DICT_Morse[letter] + ' '
-        else:
-            cipher += '/'
-    return cipher
-
-def decrypt(message):
-    message += ''
-    decipher = ''
-    citext = ' '
-    for letter in message:
-        if (letter != ''):
+message = input('Please Enter the String you wish to Encode/Decode:  ')
+# Function to encrypt the string 
+# according to the morse code chart 
+def encrypt(message): 
+    cipher = '' 
+    for letter in message: 
+        if letter != ' ': 
+  
+            # Looks up the dictionary and adds the 
+            # correspponding morse code 
+            # along with a space to separate 
+            # morse codes for different characters 
+            cipher += MORSE_CODE_DICT[letter] + ' '
+        else: 
+            # 1 space indicates different characters 
+            # and 2 indicates different words 
+            cipher += ' '
+  
+    return cipher 
+  
+# Function to decrypt the string 
+# from morse to english 
+def decrypt(message): 
+  
+    # extra space added at the end to access the 
+    # last morse code 
+    message += ' '
+  
+    decipher = '' 
+    citext = '' 
+    for letter in message: 
+  
+        # checks for space 
+        if (letter != ' '): 
+  
+            # counter to keep track of space 
             i = 0
-            citext += letter
-        else:
-             i += 1
-             if i == 2 :
+  
+            # storing morse code of a single character 
+            citext += letter 
+  
+        # in case of space 
+        else: 
+            # if i = 1 that indicates a new character 
+            i += 1
+  
+            # if i = 2 that indicates a new word 
+            if i == 2 : 
+  
+                 # adding space to separate words 
                 decipher += ' '
-             else:
-                decipher += list(DICT_Morse.keys())[list(DICT_Morse
-                .values()).index(citext)]
-                citext = ''
-    return decipher
-
-def main():
-    if encVar > 0:
-        result = decrypt(message.upper())
-        print ('Encoded Message: ',result)
-    else:
+            else: 
+  
+                # accessing the keys using their values (reverse of encryption) 
+                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT 
+                .values()).index(citext)] 
+                citext = '' 
+  
+    return decipher 
+  
+# Hard-coded driver function to run the program 
+def main(): 
+   if encVar > 0:
+       result = decrypt(message.upper())
+       print ('Encoded Message :',result)
+   else:
         result = encrypt(message.upper())
-        print ('Decoded Message: ',result)
-if __name__ == '__main__':
-    main()
+        print ('Decoded Message :',result)
+# Executes the main function 
+if __name__ == '__main__': 
+    main() 
+
